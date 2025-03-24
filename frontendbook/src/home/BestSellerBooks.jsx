78928b455b2005db5d287e93f0326a2d36daf4b0
ammar ahmed <amahmedi808@gmail.com>
2025-03-24 11:44:09 +0200

    update title

--- frontendbook/src/home/BestSellerBooks.jsx
+++ frontendbook/src/home/BestSellerBooks.jsx
@@ -9,22 +9,6 @@ const BestSellerBooks = () => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 
-    // useEffect(() => {
-    //     fetch(`${backendUrl}/all-books`)
-    //         .then(res => {
-    //             if (!res.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             const contentType = res.headers.get('content-type');
-    //             if (!contentType || !contentType.includes('application/json')) {
-    //                 throw new TypeError("Expected JSON, but got " + contentType);
-    //             }
-    //             return res.json();
-    //         })
-    //         .then(data => setBooks(data.slice(0, 8)))
-    //         .catch(error => console.error('There was a problem with the fetch operation:', error));
-    // }, []);
-
     useEffect(() => {
         fetch(`${backendUrl}/all-books`)
             .then(res => res.json())
